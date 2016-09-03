095824cd2eb89e536af20248d873b53e208dcda7
Koji Takami <vzg03566@gmail.com>
2016-09-03 16:10:00 +0900

    Refactor mz700scrn

--- MZ-700/index.js
+++ MZ-700/index.js
@@ -24,22 +24,13 @@ MZ700Js.prototype.create = function(opt) {
         //
         // MZ-700 Screen
         //
-        var onVramUpdateAll = function() {};
-        var onVramUpdate = function() {}; 
-        var screen = $(".MZ-700 .screen").mz700scrn("create", {});
-        if(screen.length > 0) {
-            (function(mz700scrn) { // This is the class object in the plugin.
-                onVramUpdateAll = function(textinfo) {
-                    Object.keys(textinfo).forEach(function(index) {
-                        var data = textinfo[index];
-                        mz700scrn.refreshChar(index, data.dispcode, data.attr);
-                    });
-                };
-                onVramUpdate = function(textinfo) {
-                    mz700scrn.refreshChar(textinfo.index, textinfo.dispcode, textinfo.attr);
-                };
-            }(screen.get(0)["mz700scrn"]));
-        }
+        var mz700scrn = (function() {
+            var screen = $(".MZ-700 .screen").mz700scrn("create", {});
+            if(screen.length > 0) {
+                return screen.get(0)["mz700scrn"];
+            }
+            return null;
+        }());
 
         //
         // Accept MZT file to drop to the MZ-700 screen, if the File API is supported.
@@ -218,8 +209,8 @@ MZ700Js.prototype.create = function(opt) {
                 'started': function() { },
                 'break': function() { this.stop(); },
 
-                'onVramUpdateAll':onVramUpdateAll,
-                'onVramUpdate':onVramUpdate,
+                'updateScreen': (mz700scrn == null) ? function() {} :
+                    function(updateData) { mz700scrn.write(updateData); },
                 'startSound': function(freq) { sound.startSound(freq); },
                 'stopSound': function() { sound.stopSound(); }
             }
