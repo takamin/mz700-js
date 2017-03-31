4c3e18964955f14285d64d209459d3a42e959083
Koji Takami <vzg03566@gmail.com>
2017-03-31 17:32:08 +0900

    Change transworker to node module

--- MZ-700/worker.js
+++ MZ-700/worker.js
@@ -5,7 +5,7 @@
 if("importScripts" in this) {
     importScripts(
             '../lib/context.js',
-            '../lib/transworker/transworker.js',
+            '../node_modules/transworker/transworker.js',
             '../lib/ex_number.js',
             '../lib/t-booster.js',
             '../lib/flip-flop-counter.js',
@@ -27,10 +27,5 @@ if("importScripts" in this) {
             'mz-data-recorder.js',
             'emulator.js');
 
-    (function() {
-        var transworker = new TransWorker();
-        var mz700 = new MZ700();
-        transworker.create(mz700);
-        mz700.setTransworker(transworker);
-    }());
+    TransWorker.create(MZ700);
 }
