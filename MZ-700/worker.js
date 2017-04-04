89bdd935ec5cae153169c41af304b858abe9fb71
Koji Takami <vzg03566@gmail.com>
2017-04-04 21:02:32 +0900

    Browserify and uglify the WebWorker script

--- MZ-700/worker.js
+++ MZ-700/worker.js
@@ -2,31 +2,13 @@
 // Codes for Worker context.
 // Override the methods in Worker context
 //
-if("importScripts" in this) {
-    importScripts(
-            '../node_modules/transworker/transworker.js',
-            '../node_modules/fractional-timer/fractional-timer.js',
-            '../lib/context.js',
-            '../lib/ex_number.js',
-            '../lib/ft-param.js',
-            '../lib/flip-flop-counter.js',
-            '../lib/ic556.js',
-            '../lib/intel-8253.js',
-            '../Z80/bin-util.js',
-            '../Z80/imem.js',
-            '../Z80/memory-block.js',
-            '../Z80/memory-bank.js',
-            '../Z80/register.js',
-            '../Z80/z80-line-assembler.js',
-            '../Z80/assembler.js',
-            '../Z80/emulator.js',
-            'monitor-rom.js',
-            'memory.js',
-            'mz700-key-matrix.js',
-            'mz-tape-header.js',
-            'mz-tape.js',
-            'mz-data-recorder.js',
-            'emulator.js');
-
+(function(g) {
+    "use strict";
+    require("../lib/context.js");
+    if(!g.context.webWorker) {
+        throw new Error("This script must run on WebWorker context.");
+    }
+    var TransWorker = require('transworker');
+    var MZ700 = require('./emulator');
     TransWorker.create(MZ700);
-}
+}(Function("return this;")()));
