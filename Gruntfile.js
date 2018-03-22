20bdf094773160e9d01fb64667101927acef8cf8
Koji Takami <vzg03566@gmail.com>
2018-03-22 23:50:22 +0900

    Install grunt-contrib-uglify-es to minify ES6 code

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks("grunt-browserify");
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.registerTask('lint',      ['eslint']);
